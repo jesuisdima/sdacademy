f81aced529fea3783d56ebe34bfade3449fe1ab0
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2016-02-24 23:27:17 +0200

    9_2 n1

--- courses/admin.py
+++ courses/admin.py
@@ -1,22 +1,16 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-'''
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    fieldsets = [(None, {'fields': ['subject', 'description', 'order']})
     ]
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    extra = 0
 
-'''
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'short_description']
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
