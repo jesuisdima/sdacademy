44110511944dc857ff32bd52c3d0d6d6bd08f652
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2016-03-11 18:24:23 +0200

    finaln2 12_2

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,7 @@ from courses import views
 
 urlpatterns = patterns('',
 url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
-url(r'^(?P<pk>\d+)/add_lesson/$', views.add_lesson, name='add-lesson'),
+url(r'^(?P<pk>\d+)/add_lesson', views.add_lesson, name='add-lesson'),
 url(r'^add/$', views.add, name='add'),
 url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
 url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
