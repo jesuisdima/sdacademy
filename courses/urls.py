35e5b03a3cb80f71c709f8b5c187da0b3d7043ae
jesuisdima <jesuisdima@yahoo.com>
2016-03-11 04:43:13 +0200

    12_2 n2

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,7 @@ from courses import views
 
 urlpatterns = patterns('',
 url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
-url(r'^(?P<pk>\d+)/add_lesson/$', views.add_lesson, name='add_lesson'),
+url(r'^(?P<pk>\d+)/add-lesson/$', views.add_lesson, name='add-lesson'),
 url(r'^add/$', views.add, name='add'),
 url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
 url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
